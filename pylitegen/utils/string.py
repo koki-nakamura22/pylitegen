aa8f200af29404d9eb9c05e58390c8df83e78d4c
Koki NAKAMURA <koki.nakamura22@gmail.com>
2022-09-02 16:42:32 +0900

    add a reference to string.py

--- pylitegen/utils/string.py
+++ pylitegen/utils/string.py
@@ -1,6 +1,8 @@
 import re
 from typing import Iterator
 
+# https://qiita.com/munepi0713/items/82ce7a56aa1b8233fd30
+
 
 def __parse_words(string: str) -> Iterator[str]:
     for block in re.split(r"[ _-]+", string):
