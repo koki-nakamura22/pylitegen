f4c631ab9811d4af70ab0ef27b35415e60609607
Koki NAKAMURA <koki.nakamura22@gmail.com>
2022-09-04 15:14:43 +0900

    delete unnecessary comments

--- example/db/db.py
+++ example/db/db.py
@@ -1,17 +1,3 @@
-#######################################
-# References
-# --------------
-# - Rails Active Record Query Interface
-#   https://guides.rubyonrails.org/active_record_querying.html
-#
-# - 【Laravel】Eloquentを使ったモデルを理解する
-#   https://bonoponz.hatenablog.com/entry/2020/10/06/%E3%80%90Laravel%E3%80%91Eloquent%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%9F%E3%83%A2%E3%83%87%E3%83%AB%E3%82%92%E7%90%86%E8%A7%A3%E3%81%99%E3%82%8B
-#
-# - Python SQLiteでデータベース接続を閉じない場合
-#   https://www.web-dev-qa-db-ja.com/ja/python/python-sqlite%E3%81%A7%E3%83%87%E3%83%BC%E3%82%BF%E3%83%99%E3%83%BC%E3%82%B9%E6%8E%A5%E7%B6%9A%E3%82%92%E9%96%89%E3%81%98%E3%81%AA%E3%81%84%E5%A0%B4%E5%90%88/942231172/
-#######################################
-
-
 import contextlib
 import sqlite3
 from sqlite3 import Connection
