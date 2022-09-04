2972ae24d3f118cda8d15f8c6b5a9bf081c9651e
Koki NAKAMURA <koki.nakamura22@gmail.com>
2022-09-04 11:19:36 +0900

    replace string assembly processing with processing that using stringbuilder

--- pylitegen/db/model_gen.py
+++ pylitegen/db/model_gen.py
@@ -3,13 +3,14 @@ from typing import List
 
 from utils.file import save_as_text
 from utils.string import to_pascal_case
+from utils.stringbuilder import StringBuilder
 from .column import Column
 
 
 def gen(table_name: str, columns: List[Column]):
     exists_any_type = False
     is_use_optional = False
-    members_code = ""
+    members_code = StringBuilder()
     for c in columns:
         if c.data_type_to_py_type_str() == 'Any':
             exists_any_type = True
@@ -20,10 +21,11 @@ def gen(table_name: str, columns: List[Column]):
             is_use_optional = True
             data_type = f"Optional[{c.data_type_to_py_type_str()}]"
 
-        members_code += f"    {c.name}: {data_type}{os.linesep}"
-    members_code += f"    table_name: str = '{table_name}'{os.linesep}"
+        members_code.append_line(f"    {c.name}: {data_type}")
+    members_code.append(f"    table_name: str = '{table_name}'")
 
-    code_str = f"from dataclasses import dataclass{os.linesep}"
+    code_str = StringBuilder()
+    code_str.append_line(f"from dataclasses import dataclass")
 
     import_typing_str = 'from typing import '
     if exists_any_type:
@@ -33,11 +35,11 @@ def gen(table_name: str, columns: List[Column]):
     import_typing_str = import_typing_str.rstrip().rstrip(',')
 
     if 19 < len(import_typing_str):
-        code_str += f"{import_typing_str}{os.linesep}"
-    code_str += os.linesep
-    code_str += os.linesep
-    code_str += f"@dataclass(init=True, eq=True, frozen=True){os.linesep}"
-    code_str += f"class {to_pascal_case(table_name)}:{os.linesep}"
-    code_str += str(members_code)
+        code_str.append_line(f"{import_typing_str}")
+    code_str.append_line('')
+    code_str.append_line('')
+    code_str.append_line("@dataclass(init=True, eq=True, frozen=True)")
+    code_str.append_line(f"class {to_pascal_case(table_name)}:")
+    code_str.append_line(members_code.to_str())
 
-    save_as_text(f"{table_name}.py", code_str)
+    save_as_text(f"{table_name}.py", code_str.to_str())
