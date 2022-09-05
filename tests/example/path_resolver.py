4ea5ecedd4c9e4555e2f5c29c2bc6d04e20b7de7
Koki NAKAMURA <koki.nakamura22@gmail.com>
2022-09-05 20:27:15 +0900

    add find function in the db class test cases

---
+++ tests/example/path_resolver.py
@@ -0,0 +1,5 @@
+from os.path import dirname, abspath
+import sys
+root_dir = dirname(dirname(dirname(abspath(__file__))))
+print(root_dir)
+sys.path.append(root_dir)
---
+++ tests/example/test_db.py
@@ -0,0 +1,103 @@
+import path_resolver
+import os
+import sys
+import pytest
+from pytest import main
+from typing import Final
+
+from example.db import DB
+from example.model import User, UserEditedHistory
+from example.script.create_test_db import DBForTestCreator
+
+currnet_dir: Final[str] = os.path.dirname(__file__)
+db_filepath: Final[str] = os.path.join(currnet_dir, 'test.db')
+
+
+class TestDB:
+
+    @classmethod
+    def setup_class(cls):
+        db_creator = DBForTestCreator(currnet_dir)
+        db_creator.create()
+
+    @classmethod
+    def teardown_class(cls):
+        if os.path.exists(db_filepath):
+            os.remove(db_filepath)
+
+    ###################
+    # Select
+    ###################
+    ###################
+    # find
+    ###################
+    def test_find_data_found(self):
+        db = DB(db_filepath)
+        with db.transaction_scope() as transaction:
+            user = User(1, 'TestUser', '123', 'Japan')
+            transaction.insert(user)
+            user2 = User(2, 'TestUser2', '456', 'Japan')
+            transaction.insert(user2)
+
+            found_user = transaction.find(User, {'id': 1})
+            assert isinstance(found_user, User)
+            assert found_user == user
+
+    def test_find_data_not_found(self):
+        db = DB(db_filepath)
+        with db.transaction_scope() as transaction:
+            found_user = transaction.find(User, {'id': 1})
+            assert found_user is None
+
+    def test_find_primary_keys_and_condition_do_not_match(self):
+        db = DB(db_filepath)
+        with db.transaction_scope() as transaction:
+            user = User(1, 'TestUser', '123', 'Japan')
+            transaction.insert(user)
+
+            with pytest.raises(ValueError) as e:
+                found_user = transaction.find(
+                    User, {'id': 1, 'name': 'TestUser'})
+            assert str(e.value) == 'Primary keys do not match'
+
+    def test_find_no_primary_key_model(self):
+        db = DB(db_filepath)
+        with db.transaction_scope() as transaction:
+            user_edited_history = UserEditedHistory(
+                '2022/10/31 10:12:34', 'aaa')
+            transaction.insert(user_edited_history)
+
+            with pytest.raises(ValueError) as e:
+                found_data = transaction.find(
+                    UserEditedHistory, {
+                        'date': '2022/10/31 10:12:34'})
+            assert str(e.value) == 'Primary keys do not match'
+
+    ###################
+    # find_by
+    ###################
+    def test_find_by_data_found(self):
+        pass
+
+    def test_find_by_data_not_found(self):
+        pass
+
+    ###################
+    # Insert
+    ###################
+
+    ###################
+    # Update
+    ###################
+
+    ###################
+    # Delete
+    ###################
+
+    ###################
+    # Transaction
+    ###################
+
+
+if __name__ == '__main__':
+    sys.exit(main())
