96ac347af9c8873a4274d6be3f515f878f560895
Koki NAKAMURA <koki.nakamura22@gmail.com>
2022-09-14 17:42:07 +0900

    implements a main entry for command

--- pyqlite/main.py
+++ pyqlite/main.py
@@ -1,25 +1,41 @@
-import argparse
-from ast import parse
+from argparse import ArgumentParser
+import os
 import sys
 
+from pyqlite.generator import Generator
 
-def main():
-    # args = sys.argv
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-g model', '--generate model', help='test help')
-    parser.add_argument('-d', '--db-path', help='DB file path')
-    parser.add_argument('-o', '--output-path', help='Model files output path')
+def main() -> int:
+    parser = ArgumentParser()
+    parser.add_argument(
+        '-gm',
+        '--generate-model',
+        required=True,
+        action='store_true',
+        help='Flag to create model files from a db file. Must use with -d or --db-path.')
+    parser.add_argument(
+        '-d',
+        '--db-path',
+        required=True,
+        help='Specify a db file path that is used to create model files. Must use with -g model or --generate model')
+    parser.add_argument(
+        '-o',
+        '--output-path',
+        help='A created model files output path.')
 
-    argv = ['this file path', '-g model', 'nya-n']
-    # args = parser.parse_args(argv[1:])
-    args = parser.parse_args(sys.argv[1:])
-    print(args)
-    if 'generate model' in args:
-        pass
-    else:
-        pass
-        # error
+    try:
+        args = parser.parse_args(sys.argv[1:])
+        if args.output_path is None:
+            output_path = os.getcwd()
+        else:
+            output_path = args.output_path
+        Generator.generate(args.db_path, output_path)
+        return 0
+
+    except Exception as e:
+        from pprint import pprint
+        pprint(e)
+        return 1
 
 
 if __name__ == '__main__':
